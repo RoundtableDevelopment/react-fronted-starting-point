ef186a0d7aa22a5c43fd8863ba84edd891e5119e
dmcbrayer <dmcbrayer@gmail.com>
2015-12-20 23:11:03 -0600

    Adds some stuff to gitignore

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import AppBar from 'material-ui/lib/app-bar';
 import RaisedButton from 'material-ui/lib/raised-button';
-import Image from './image';
 
 export default class Main extends React.Component {
     render() {
         return (
           <div>
-            <AppBar
-              title="Title"
-              iconClassNameRight="muidocs-icon-navigation-expand-more" />
             <h1>This is the main thing</h1>
-            <RaisedButton label="Default" />
           </div>
         )
     }
