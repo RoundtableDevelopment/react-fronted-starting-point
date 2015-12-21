2d49f8d61b720cda569ec8288c84c325da4df24c
dmcbrayer <dmcbrayer@gmail.com>
2015-12-20 18:13:38 -0600

    Added a few more advanced things to the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
 var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var sass        = require('gulp-sass');
+var minifyCSS   = require('gulp-minify-css');
 var browserify  = require('browserify');
 var babelify    = require('babelify');
 var streamify   = require('gulp-streamify');
@@ -8,21 +11,34 @@ var uglify      = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 var reload      = browserSync.reload;
 
-gulp.task('build', function () {
-  return browserify({entries: './app/scripts/app.jsx', extensions: ['.jsx'], debug: true})
+var paths = {
+  scripts: './app/scripts/app.jsx',
+  stylesheets: './app/stylesheets/*.scss',
+}
+
+gulp.task('buildJS', ['clean'], function () {
+  return browserify({entries: paths.scripts, extensions: ['.jsx'], debug: true})
       .transform('babelify', {presets: ['es2015', 'react']})
       .bundle()
       .pipe(source('bundle.js'))
-      .pipe(streamify(uglify()))
+      .pipe(gutil.env.type === 'production' ? streamify(uglify()) : gutil.noop())
       .pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', function(cb) {
-  del(['dist'], cb);
+gulp.task('buildCSS', ['clean'], function() {
+  return gulp.src(paths.stylesheets)
+    .pipe(sass())
+    .pipe(gutil.env.type === 'production' ? minifyCSS() : gutil.noop())
+    .pipe(gulp.dest('dist'))
+})
+
+gulp.task('clean', function() {
+  return del(['dist']);
 });
 
-gulp.task('watch', ['build'], function () {
-  gulp.watch('./app/scripts/*.jsx', ['build', reload]);
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['buildJS', reload]);
+  gulp.watch(paths.stylesheets, ['buildCSS', reload]);
 });
 
 gulp.task('browser-sync', function() {
@@ -33,4 +49,6 @@ gulp.task('browser-sync', function() {
   });
 });
 
-gulp.task('default', ['watch','browser-sync']);
+gulp.task('default', ['watch', 'buildJS', 'buildCSS', 'browser-sync'], function() {
+  gutil.log('stuff happened', 'Really it did', gutil.colors.magenta('123'));
+});
